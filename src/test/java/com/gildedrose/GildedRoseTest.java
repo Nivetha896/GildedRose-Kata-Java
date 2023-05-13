97d31588429b3b7c0d0adae7545185fbecdb276b
Nivetha896 <81308563+Nivetha896@users.noreply.github.com>
2023-05-13 11:27:04 +0530

    Extracting the logic of Aged Brie in single if statement

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,33 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	// Aged Brie
+	@Test
+	public void quality_increases_at_the_end_of_each_day() throws Exception {
+		Item[] items = new Item[] { new Item("Aged Brie", 10, 10) };
+		GildedRose app = new GildedRose(items);
+
+		app.updateQuality();
+
+		assertEquals(9, app.items[0].sellIn);
+		assertEquals(11, app.items[0].quality);
+	}
+
+	// Aged Brie
+	@Test
+	public void quality_above_50_never_increases() throws Exception {
+		Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+		GildedRose app = new GildedRose(items);
+
+		app.updateQuality();
+
+		assertEquals(9, app.items[0].sellIn);
+		assertEquals(50, app.items[0].quality);
+	}
 
 }
