dad2ec6e76aaa17f6164733b4ab74d34eeac9ead
Nivetha896 <81308563+Nivetha896@users.noreply.github.com>
2023-05-13 12:10:20 +0530

    Implement conjured items functionality

--- src/main/java/com/gildedrose/ItemUpdateHandler.java
+++ src/main/java/com/gildedrose/ItemUpdateHandler.java
@@ -3,18 +3,38 @@ package com.gildedrose;
 public class ItemUpdateHandler {
 
 	public static Updater updaterFor(Item currentItem) {
-		if (currentItem.name.equals("Aged Brie")) {
+		if (improvesWithAge(currentItem)) {
 			return new AgingItemController();
 		}
-		else if(currentItem.name.equals("Sulfuras, Hand of Ragnaros")) {
+		else if(isLegendary(currentItem)) {
 			return new SulfurasController();
 		}
-		else if(currentItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+		else if(isBackstagePass(currentItem)) {
 			return new BackstagePassesController();
 		}
+		else if(isConjured(currentItem)) {
+			return new ConjuredItemController();
+		}
 		else {
-			return new CommonItemController();    			
+			return new CommonItemController();    	   			
 		}
 	}
+	
+	private static boolean isBackstagePass(Item item) {
+		return "Backstage passes to a TAFKAL80ETC concert".equals(item.name);
+	}
+
+	private static boolean isLegendary(Item item) {
+		return "Sulfuras, Hand of Ragnaros".equals(item.name);
+	}
+
+	private static boolean improvesWithAge(Item item) {
+		return "Aged Brie".equals(item.name);
+	}
+	private static boolean isConjured(Item item) {
+		return "Conjured Mana Cake".equals(item.name);
+	}
+
+
 
 }
