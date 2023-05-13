78ba8cf95a6ef76bf15fcd9056313cb3066696da
Nivetha896 <81308563+Nivetha896@users.noreply.github.com>
2023-05-13 11:54:15 +0530

    Update with separate methods for each item type

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,42 +10,52 @@ class GildedRose {
 	public void updateQuality() {
 		for (Item currentItem : items) {
 			if (currentItem.name.equals("Aged Brie")) {
-				if (currentItem.quality < 50) {
-					currentItem.quality++;
-				}
-				currentItem.sellIn--;
+				updateAgedBrie(currentItem);
 
 			} else if (currentItem.name.equals("Sulfuras, Hand of Ragnaros")) {
 
 			} else if (currentItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (currentItem.sellIn <= 0) {
-					currentItem.quality = 0;
-				} else if (currentItem.sellIn <= 5) {
-					if (currentItem.quality < 50) {
-						currentItem.quality += 3;
-					}
-				} else if (currentItem.sellIn <= 10) {
-					if (currentItem.quality < 50) {
-						currentItem.quality += 2;
-					}
-				} else {
-					if (currentItem.quality < 50) {
-						currentItem.quality++;
-					}
-				}
-				currentItem.sellIn--;
-
+				updateBackstagePasses(currentItem);
 			} else {
-				if (currentItem.quality > 0) {
-					currentItem.quality--;
-				}
-				currentItem.sellIn--;
-
-				if (currentItem.sellIn < 0 && currentItem.quality > 0) {
-					currentItem.quality--;
-
-				}
+				updateCommonItem(currentItem);
 			}
+		}
+	}
+
+	private void updateAgedBrie(Item currentItem) {
+		if (currentItem.quality < 50) {
+			currentItem.quality++;
+		}
+		currentItem.sellIn--;
+	}
+
+	private void updateBackstagePasses(Item currentItem) {
+		if (currentItem.sellIn <= 0) {
+			currentItem.quality = 0;
+		} else if (currentItem.sellIn <= 5) {
+			if (currentItem.quality < 50) {
+				currentItem.quality += 3;
+			}
+		} else if (currentItem.sellIn <= 10) {
+			if (currentItem.quality < 50) {
+				currentItem.quality += 2;
+			}
+		} else {
+			if (currentItem.quality < 50) {
+				currentItem.quality++;
+			}
+		}
+		currentItem.sellIn--;
+	}
+
+	private void updateCommonItem(Item currentItem) {
+		if (currentItem.quality > 0) {
+			currentItem.quality--;
+		}
+		currentItem.sellIn--;
+		
+		if (currentItem.sellIn < 0 && currentItem.quality > 0) {
+			currentItem.quality--;
 		}
 	}
 }
